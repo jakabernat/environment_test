d5be129ce230cdf124a40d901765a00ce5761c5c
ecyr20 <ecyr20@tamu.edu>
2023-09-12 08:31:32 -0500

    Seed file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create([
+    {title: 'Harry Potter', author: 'J.K. Rowling', price: 10.99, publishedDate: '2004-08-05'},
+    {title: 'Eragon', author: 'Christopher Paolini', price: 13.99, publishedDate: '2009-03-02'},
+    {title: 'Lord of the Rings', author: 'Forgor', price: 12.99, publishedDate: '2023-06-15'},
+    {title: 'Game of Thrones', author: 'George RR. Martin', price: 20.99, publishedDate: '2000-07-25'},
+    {title: 'Name of the Wind', author: 'John Doe', price: 16.56, publishedDate: '2012-01-05'},
+])
